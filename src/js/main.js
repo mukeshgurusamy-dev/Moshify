c02c23443b55ea58b02538fc638f15bd21f86fc4
Mukesh Gurusamy <mukeshgurusamy.dev@gmail.com>
2025-08-08 22:50:16 +0530

    Adding comments in main.js.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+/*This line tells Parcel to include the SVG file in the final dist folder during build.*/
+import "../images/sprite.svg";
+
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
